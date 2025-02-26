1a6a6b2206f5fc3be31a77bdc4c3d8cfe9658c6f
Catargo <cobauper@gmail.com>
2025-02-26 19:18:26 +0100

    fixed iteration and added that if the code of the character is less or 127 it's going to use it

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,26 @@
 //!optimize 2
 import data from "./data";
 
-const lookup_map = new Map<string, string>();
+const lookup_map = new Map<number, string>();
 data.split("\n").map((line) => {
 	const [transcription, unicodes_text] = line.split("\t");
 	if (transcription === undefined) return;
 	if (unicodes_text === undefined) return;
 	const unicodes = unicodes_text.split(",");
 	for (const unicode of unicodes) {
-		lookup_map.set(unicode, transcription);
+		lookup_map.set(utf8.codepoint(unicode)[0], transcription);
 	}
 });
 
 export function Asciify(text: string): string {
-	return text
-		.split("")
-		.map((char) => lookup_map.get(char) ?? "")
-		.join("");
+	const strings: string[] = [];
+	for (const [index, char_number] of utf8.codes(text)) {
+		if (char_number <= 127) {
+			strings.push(string.char(char_number));
+			continue;
+		}
+		strings.push(lookup_map.get(char_number) ?? "#");
+	}
+
+	return strings.join("");
 }
